8930b254f1f9235b97ccb3ee0584dfaef2e90ed3
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-05-05 12:31:36 +0100

    Add unit tests to test default settings.

---
+++ test/unit-tests/main.tf
@@ -0,0 +1,16 @@
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
